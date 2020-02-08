1c080000a8ac8569ca94fa034888b00f50240ca2
carterDWatts <cdwatts2001@gmail.com>
2020-02-08 18:50:19 -0500

    Backed up through script

--- scripts/downloadsNecessities.sh
+++ scripts/downloadsNecessities.sh
@@ -1,4 +1,9 @@
-sudo pacman -S xorg xorg-xinit qutebrowser steam filezilla arduino blueman discord yay vim scrot ranger powertop i3 dmenu bluez alsa-utils acpi
+sudo pacman -S xorg xorg-xinit qutebrowser steam filezilla arduino blueman discord yay vim scrot ranger powertop i3 dmenu bluez alsa-utils acpi git
 
+cd /home/carter/dev/git/git
+git clone https://aur.archlinux.org/yay.git 
+cd /home/carter/dev/git/yay
+makepkg -si
+cd /home/carter/
 yay -S apvlv polybar spotify eagle
 
