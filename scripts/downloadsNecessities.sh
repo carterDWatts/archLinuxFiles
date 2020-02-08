6fd93d8d8392954f30f4f172a1bead308c3d48fa
carterDWatts <cdwatts2001@gmail.com>
2020-02-08 18:39:28 -0500

    Backed up through script

---
+++ scripts/downloadsNecessities.sh
@@ -0,0 +1,4 @@
+sudo pacman -S xorg xorg-xinit qutebrowser steam filezilla arduino blueman discord yay vim scrot ranger powertop i3 dmenu bluez alsa-utils acpi
+
+yay -S apvlv polybar spotify eagle
+
