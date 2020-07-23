5f49b8cd6379ec3b84a9ff3a748d74b0cbb76dce
carterDWatts <cdwatts2001@gmail.com>
2020-07-23 15:42:32 -0400

    Backed up through script

---
+++ scripts/writeBackup.sh
@@ -0,0 +1,10 @@
+cp -r /home/carter/dev/git/archLinuxFiles/i3 /home/carter/.config/i3
+cp -r /home/carter/dev/git/archLinuxFiles/polybar /home/carter/.config/polybar
+
+cp /home/carter/dev/git/archLinuxFiles/homeFiles/.xinitrc /home/carter/.xinitrc
+cp /home/carter/dev/git/archLinuxFiles/homeFiles/.Xresources /home/carter/.Xresources
+cp /home/carter/dev/git/archLinuxFiles/homeFiles/.bashrc /home/carter/.bashrc
+
+cp /home/carter/dev/git/archLinuxFiles/vim/.vimrc /home/carter/.vimrc
+cp /home/carter/dev/git/archLinuxFiles/nvim /home/carter/.config/nvim
+
