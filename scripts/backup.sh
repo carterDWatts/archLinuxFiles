348a99e8b9855a518e719895d5e8b4003d3e9df8
carterDWatts <cdwatts2001@gmail.com>
2020-12-09 14:34:08 -0500

    Backed up through script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,7 +4,7 @@ cp /home/carter/.Xresources /home/carter/dev/git/archLinuxFiles/homeFiles
 cp /home/carter/.bashrc /home/carter/dev/git/archLinuxFiles/homeFiles
 cp /home/carter/.xbindkeysrc /home/carter/dev/git/archLinuxFiles/homeFiles
 cp /home/carter/.Xmodmap /home/carter/dev/git/archLinuxFiles/homeFiles
-cp -r /home/carter/.vim /home/carter/dev/git/archLinuxFiles/homeFiles
+#cp -r /home/carter/.vim /home/carter/dev/git/archLinuxFiles/homeFiles
 
 #save config folder
 cp -r /home/carter/.config/* /home/carter/dev/git/archLinuxFiles/config
